0acaf737338c3d6ca9df4fd829d5fb246e3f8b03
Taazz7 <garciacbzc@gmail.com>
2026-02-23 17:50:19 +0100

    dependencias vuetify

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,14 +1,25 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+
+// Importar Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi' // Para los iconos
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: { defaultTheme: 'dark' } // O 'light' según prefieras
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify) // ¡Esto es vital!
 
 app.mount('#app')
